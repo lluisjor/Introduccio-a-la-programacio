8c1949dbbdae842ac272d59cff1e572b5451af20
lluisjor <158268588+lluisjor@users.noreply.github.com>
2024-02-16 09:53:31 +0100

    Update  bateria6

---
+++ bateria6.py
@@ -0,0 +1,7 @@
+nom_usuari = input("Introdueix el teu nom: ")
+
+if nom_usuari == "Amilcar" or nom_usuari == "Sila":
+    print("Benvingut, " + nom_usuari + "!")
+else:
+    print("Nom d'usuari no vàlid. Torna-ho a provar.")
+  
